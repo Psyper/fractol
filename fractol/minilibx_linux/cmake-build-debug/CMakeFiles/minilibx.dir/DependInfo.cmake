41be9453b3b89a1dcdd9949538a9118f1040ff40
Psyper <psycostin@gmail.com>
2021-02-26 11:15:15 +0300

    Add files via upload

---
+++ fractol/minilibx_linux/cmake-build-debug/CMakeFiles/minilibx.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/psy/fractol/minilibx/mlx_clear_window.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_clear_window.c.o"
+  "/home/psy/fractol/minilibx/mlx_destroy_image.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_destroy_image.c.o"
+  "/home/psy/fractol/minilibx/mlx_destroy_window.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_destroy_window.c.o"
+  "/home/psy/fractol/minilibx/mlx_expose_hook.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_expose_hook.c.o"
+  "/home/psy/fractol/minilibx/mlx_flush_event.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_flush_event.c.o"
+  "/home/psy/fractol/minilibx/mlx_get_color_value.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_get_color_value.c.o"
+  "/home/psy/fractol/minilibx/mlx_get_data_addr.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_get_data_addr.c.o"
+  "/home/psy/fractol/minilibx/mlx_hook.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_hook.c.o"
+  "/home/psy/fractol/minilibx/mlx_init.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_init.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_anti_resize_win.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_anti_resize_win.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_do_nothing.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_do_nothing.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_get_visual.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_get_visual.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_param_event.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_param_event.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_set_win_event_mask.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_set_win_event_mask.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_str_to_wordtab.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_str_to_wordtab.c.o"
+  "/home/psy/fractol/minilibx/mlx_int_wait_first_expose.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_int_wait_first_expose.c.o"
+  "/home/psy/fractol/minilibx/mlx_key_hook.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_key_hook.c.o"
+  "/home/psy/fractol/minilibx/mlx_lib_xpm.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_lib_xpm.c.o"
+  "/home/psy/fractol/minilibx/mlx_loop.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_loop.c.o"
+  "/home/psy/fractol/minilibx/mlx_loop_hook.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_loop_hook.c.o"
+  "/home/psy/fractol/minilibx/mlx_mouse_hook.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_mouse_hook.c.o"
+  "/home/psy/fractol/minilibx/mlx_new_image.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_new_image.c.o"
+  "/home/psy/fractol/minilibx/mlx_new_window.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_new_window.c.o"
+  "/home/psy/fractol/minilibx/mlx_pixel_put.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_pixel_put.c.o"
+  "/home/psy/fractol/minilibx/mlx_put_image_to_window.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_put_image_to_window.c.o"
+  "/home/psy/fractol/minilibx/mlx_rgb.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_rgb.c.o"
+  "/home/psy/fractol/minilibx/mlx_string_put.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_string_put.c.o"
+  "/home/psy/fractol/minilibx/mlx_xpm.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/mlx_xpm.c.o"
+  "/home/psy/fractol/minilibx/test/main.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/test/main.c.o"
+  "/home/psy/fractol/minilibx/test/new_win.c" "/home/psy/fractol/minilibx/cmake-build-debug/CMakeFiles/minilibx.dir/test/new_win.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ fractol/minilibx_linux/cmake-build-debug/CMakeFiles/minilibx.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minilibx.dir/mlx_clear_window.c.o"
+  "CMakeFiles/minilibx.dir/mlx_destroy_image.c.o"
+  "CMakeFiles/minilibx.dir/mlx_destroy_window.c.o"
+  "CMakeFiles/minilibx.dir/mlx_expose_hook.c.o"
+  "CMakeFiles/minilibx.dir/mlx_flush_event.c.o"
+  "CMakeFiles/minilibx.dir/mlx_get_color_value.c.o"
+  "CMakeFiles/minilibx.dir/mlx_get_data_addr.c.o"
+  "CMakeFiles/minilibx.dir/mlx_hook.c.o"
+  "CMakeFiles/minilibx.dir/mlx_init.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_anti_resize_win.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_do_nothing.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_get_visual.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_param_event.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_set_win_event_mask.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_str_to_wordtab.c.o"
+  "CMakeFiles/minilibx.dir/mlx_int_wait_first_expose.c.o"
+  "CMakeFiles/minilibx.dir/mlx_key_hook.c.o"
+  "CMakeFiles/minilibx.dir/mlx_lib_xpm.c.o"
+  "CMakeFiles/minilibx.dir/mlx_loop.c.o"
+  "CMakeFiles/minilibx.dir/mlx_loop_hook.c.o"
+  "CMakeFiles/minilibx.dir/mlx_mouse_hook.c.o"
+  "CMakeFiles/minilibx.dir/mlx_new_image.c.o"
+  "CMakeFiles/minilibx.dir/mlx_new_window.c.o"
+  "CMakeFiles/minilibx.dir/mlx_pixel_put.c.o"
+  "CMakeFiles/minilibx.dir/mlx_put_image_to_window.c.o"
+  "CMakeFiles/minilibx.dir/mlx_rgb.c.o"
+  "CMakeFiles/minilibx.dir/mlx_string_put.c.o"
+  "CMakeFiles/minilibx.dir/mlx_xpm.c.o"
+  "CMakeFiles/minilibx.dir/test/main.c.o"
+  "CMakeFiles/minilibx.dir/test/new_win.c.o"
+  "minilibx"
+  "minilibx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minilibx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
