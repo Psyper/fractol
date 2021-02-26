80821ed00505c98c4ce15a81954bd531ddcbf642
Psyper <psycostin@gmail.com>
2021-02-26 11:18:06 +0300

    Add files via upload

---
+++ fractol/cmake-build-debug/CMakeFiles/fractol.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/psy/CLionProjects/fractol_gh/fractol/draw.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/draw.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/fractal.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/fractal.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/ft_putstr.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/ft_putstr.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/ft_strcmp.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/ft_strcmp.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/initialize.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/initialize.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/linux_controls.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/linux_controls.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/main.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/main.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/operations.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/operations.c.o"
+  "/home/psy/CLionProjects/fractol_gh/fractol/threads.c" "/home/psy/CLionProjects/fractol_gh/fractol/cmake-build-debug/CMakeFiles/fractol.dir/threads.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ fractol/cmake-build-debug/CMakeFiles/fractol.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fractol.dir/draw.c.o"
+  "CMakeFiles/fractol.dir/fractal.c.o"
+  "CMakeFiles/fractol.dir/ft_putstr.c.o"
+  "CMakeFiles/fractol.dir/ft_strcmp.c.o"
+  "CMakeFiles/fractol.dir/initialize.c.o"
+  "CMakeFiles/fractol.dir/linux_controls.c.o"
+  "CMakeFiles/fractol.dir/main.c.o"
+  "CMakeFiles/fractol.dir/operations.c.o"
+  "CMakeFiles/fractol.dir/threads.c.o"
+  "fractol"
+  "fractol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fractol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
